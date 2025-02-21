7acc362224c66dda2bcf9711610e6a6f14f2fd5e
avernikoz <avernikoz@gmail.com>
2025-02-21 21:27:55 +0300

    refactor(url_fetcher): make fetchUrl public for testability
    Changes fetchUrl from private to public to enable better test scoping with vi.spyOn.
    This allows for more granular control over mocking in individual test cases.

--- portal/common/lib/url_fetcher.ts
+++ portal/common/lib/url_fetcher.ts
@@ -157,7 +157,7 @@ export class UrlFetcher {
      * @param objectId - The object ID of the site object.
      * @param path - The path of the site resource to fetch. e.g. /index.html
      */
-    private async fetchUrl(
+    public async fetchUrl(
         objectId: string,
         path: string,
     ): Promise<Response> {
